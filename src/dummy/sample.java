f58bb97f87314978a562744e9afaa7864a459e13
Sushil Ghodke <Sushil Ghodke@192.168.1.39>
2022-08-20 10:31:34 +0530

    [Sushil] Added new package and class

---
+++ src/dummy/sample.java
@@ -0,0 +1,16 @@
+package dummy;
+
+public class sample {
+	
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("Sushil");
+		
+		System.out.println("How are you");
+		
+		System.out.println("I'm good");
+	}
+
+}
